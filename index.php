388df2ce40fe51bd94be1e712b56f4ec57cfa295
Laura Kantti <laura.kantti@gmail.com>
2014-12-10 13:56:48 +0100

    index.php
    Adding back until fix

--- index.php
+++ index.php
@@ -1,3 +1,6 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+
 <?php
 ini_set('display_startup_errors',1);
 ini_set('display_errors',1);
@@ -88,9 +91,79 @@ if ( isset( $session ) ) {
 
   include("templates/footer.php");
 
-} else {
+} else {?>
 
-    include("templates/welcome.php");
+    <!doctype html>
+    <html class="no-js" lang="en">
+      <head>
+        <title>Nacka Forum</title>
 
+        <!-- META -->
+        <meta charset="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+
+        <!-- CSS -->
+        <link rel="stylesheet" href="css/vendor/foundation.min.css" />
+        <link rel="stylesheet" href="css/vendor/foundation-icons.css">
+        <link rel="stylesheet" href="css/style.css">
+
+        <!-- FONTS -->
+        <link href='http://fonts.googleapis.com/css?family=Josefin+Sans:400,600,700|Raleway:400' rel='stylesheet' type='text/css'>
+
+        <!-- MODERNIZR -->
+        <script src="js/vendor/modernizr.js"></script>
+      </head>
+
+      <body>
+
+        <div id="wrapper">
+          <div class="container" id="container-welcome">
+            <div class="page-welcome" id="section-welcome">
+                <div class="background-image"></div>
+
+                <section>
+                    <h2>Ladda upp din bästa matbild</h2>
+                    <h3>Vinn en matkasse med varor utvalda av kocken Tommy Myllymäki</h3>
+                </section>
+            </div>
+            <div class="small-4 columns rules-column">
+                <div class="column-inner">
+                    <div class="column-border"></div>
+                        <h4>Lägg till appen för att gå vidare</h4>
+                        <?php
+                          // login url [installera app]
+                          $helper = new FacebookRedirectLoginHelper('https://www.facebook.com/nackaforumChen/app_1386865751606193');
+                          echo '<a href="' . $helper->getLoginUrl( array( 'email', 'user_friends' ) ) . '" target="_top">KLICKA HÄR</a>';
+                        ?>
+                </div>
+            </div>
+
+            <footer id="welcome-footer">
+                <div class="logo"></div>
+            </footer>
+
+          </div>
+        </div>
+
+        <script src="js/vendor/jquery-1.11.1.min.js"></script>
+        <script src="js/vendor/foundation.min.js"></script>
+
+        <script src="js/main.js"></script>
+
+      </body>
+    </html>
+
+<?php
 }
+/////////////////  end of welcome.php//////////////////////
 
+
+?>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>Untitled Document</title>
+</head>
+
+<body>
+</body>
+</html>
